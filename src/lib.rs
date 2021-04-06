bc72c6b3ef00e9a2e4e9871c36dda4720a181032
Rita Kitic <rikitau@protonmail.com>
2021-04-07 01:04:43 +0200

    return error instead of panic

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,25 @@ use std::str::FromStr;
 use bitcoin::secp256k1::Secp256k1;
 use bitcoin::util::bip32::ExtendedPrivKey;
 
+// https://github.com/rustwasm/wasm-bindgen/issues/1742#issuecomment-643793491
+macro_rules! jserr {
+    ($expression:expr) => {
+        match $expression {
+            Ok(a) => a,
+            Err(e) => {
+                return Err(JsValue::from(format!("{}", e)));
+            }
+        }
+    };
+}
+
 #[wasm_bindgen]
 pub fn to_wif(xprv: &str,
               idx: u32
               ) -> Result<JsValue, JsValue>{
     let secp = Secp256k1::new();
-    let root = ExtendedPrivKey::from_str(xprv).expect("invalid xprv");
-    let wif = bip85::to_wif(&secp, &root, idx).expect("cant convert");
+    let root = jserr!(ExtendedPrivKey::from_str(xprv));
+    let wif = jserr!(bip85::to_wif(&secp, &root, idx));
     Ok(wif.to_string().into())
 }
 
@@ -21,7 +33,8 @@ pub fn to_mnemonic(xprv: &str,
                    idx: u32
                 ) -> Result<JsValue, JsValue>{
     let secp = Secp256k1::new();
-    let root = ExtendedPrivKey::from_str(xprv).expect("invalid xprv");
-    let mn = bip85::to_mnemonic(&secp, &root, words_number, idx).expect("cant convert, invalid words number or index?");
+    let root = jserr!(ExtendedPrivKey::from_str(xprv));
+    let mn = jserr!(bip85::to_mnemonic(&secp, &root, words_number, idx));
     Ok(mn.to_string().into())
 }
+
